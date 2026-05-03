ed38e4ac4efa4024e787a233bff044c19c7fe138
Rudra Patel <rp68@email.sc.edu>
2026-05-03 17:00:34 -0400

    File Reupload

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './PlatformCard';
-export * from './PlatformGrid';
-export * from './ScoreHeader';
-export * from './JDInput';
-export * from './ResumeTextInput';
-// Other components can be imported/exported here as built.
--- src/lib/scorer/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types';
-export * from './engine';
-export * from './formatScorer';
-export * from './keywordScorer';
-export * from './sectionScorer';
-export * from './experienceScorer';
-export * from './educationScorer';
